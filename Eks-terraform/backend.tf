e44b7fefad07f9b102acec636dc4f8ef14ee39d5
anuyadav2602 <anuyadanaveni2602@gmail.com>
2024-06-03 12:42:06 +0530

    region

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "mybuckettetris" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "us-east-1"
+    region = "ap-south-1"
   }
 }
 
